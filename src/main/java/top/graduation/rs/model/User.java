e49accfc1f61b438127a0b1d8b6772b9d8761440
Joanna Pakosh <morlineday@ukr.net>
2018-09-26 07:12:28 +0300

    unused imports in User

--- src/main/java/top/graduation/rs/model/User.java
+++ src/main/java/top/graduation/rs/model/User.java
@@ -1,9 +1,5 @@
 package top.graduation.rs.model;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.CollectionUtils;
 
 import javax.persistence.*;
