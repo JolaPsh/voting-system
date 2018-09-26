4ed0cff93687c4f7267ee92c3b1e2e52d439d12f
Joanna Pakosh <morlineday@ukr.net>
2018-09-26 07:28:40 +0300

    UserServiceSecurity: finished, configuring authenticated email as username

--- src/main/java/top/graduation/rs/service/UserServiceSecurity.java
+++ src/main/java/top/graduation/rs/service/UserServiceSecurity.java
@@ -3,13 +3,14 @@ package top.graduation.rs.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import top.graduation.rs.AuthorizedUser;
 import top.graduation.rs.model.User;
-import top.graduation.rs.repository.UserRepository;
+import top.graduation.rs.repository.datajpa.UserRepository;
 
 /**
  * Created by Joanna Pakosh on Сент., 2018
@@ -18,15 +19,21 @@ import top.graduation.rs.repository.UserRepository;
 @Service
 public class UserServiceSecurity implements UserDetailsService {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger log = LoggerFactory.getLogger(UserServiceSecurity.class);
 
     @Autowired
     private UserRepository repository;
 
+    /**
+     * Returns user with specified email;
+     * @param email of user that should be returned
+     * @return user with specified email;
+     */
     @Override
-    @Transactional(readOnly = true)
-    public AuthorizedUser loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = repository.findUserByEmail(email);
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email.toLowerCase());
+        log.debug("Authenticating {}", email);
         if (user == null) {
             throw new UsernameNotFoundException("User with email" + email + " not found");
         }
