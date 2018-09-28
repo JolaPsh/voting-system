36e514b7def200f6b95796cf770f50322d1e9c51
Joanna Pakosh <morlineday@ukr.net>
2018-09-28 23:14:21 +0300

    README.md updated

--- README.md
+++ README.md
@@ -33,13 +33,18 @@ curl -s -X POST -d '{ "title": "Tiget", "location": "3A Lenina Street"}' -H 'Con
 UPDATE restaurant: PUT
 curl -s -X PUT -d '{"id": 1004, "title":"FRONTO", "location":"3 Chapel Street, Lviv"}' -H 'Content-Type: application/json' http://localhost:8080/rest/admin/restaurants/1004 --user admin@gmail.com:admin
 
-dishes:
-localhost:8080/rest/admin/dishes GET
-localhost:8080/rest/admin/dishes/1002 GET
-localhost:8080/rest/admin/dishes/1002 DELETE
-localhost:8080/rest/admin/dishes POST
-localhost:8080/rest/admin/dishes/1002 PUT
+**dishes**:
 
+GET all dishes:
+curl -s http://localhost:8080/rest/admin/dishes --user admin@gmail.com:admin
+GET dish with id 1013
+curl -s http://localhost:8080/rest/admin/dishes/1013 --user admin@gmail.com:admin
+DELETE dish with id 1013:
+ curl -s -X DELETE http://localhost:8080/rest/admin/dishes/1010 --user admin@gmail.com:admin
+CREATE dish: POST
+curl -s -X POST -d '{"date":"2018-09-20", "name": "Shrimp&vegetables", "price": 84, "restaurant": {"id": 1002, "title": "Kruivka", "location": "11 Mykolaja street, Ternopil"}}' -H 'Content-Type:application/json;charset=UTF-8' http://localhost:8080/rest/admin/dishes --user admin@gmail.com:admin
+UPDATE dish: PUT
+curl -s -X PUT -d '{"id": 1015, "date":"2018-09-20", "name":"Cocoa", "price": 72, "restaurant": {"id": 1000, "title": "Local", "location": "33 Dark Spurt, Lviv"}}' -H 'Content-Type: application/json' http://localhost:8080/rest/admin/dishes/1015 --user admin@gmail.com:admin
 
 user actions:
 rest/profile/restaurants GET
--- src/test/java/top/graduation/rs/TestUtil.java
+++ src/test/java/top/graduation/rs/TestUtil.java
@@ -1,10 +1,20 @@
 package top.graduation.rs;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import top.graduation.rs.model.Restaurant;
+import top.graduation.rs.model.User;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static top.graduation.rs.web.json.JsonUtil.writeValue;
+
+// nie ma
+//import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 
 /**
  * Created by Joanna Pakosh on Сент., 2018
@@ -22,4 +32,21 @@ public class TestUtil {
     public static void assertMatch(Iterable<Restaurant> actual, Iterable<Restaurant> expected) {
         assertThat(actual).isEqualTo(expected);
     }
+
+    public static RequestPostProcessor userHttpBasic(User user) {
+      //  return SecurityMockMvcRequestPostProcessors.httpBasic(user.getEmail(), user.getPassword());
+        return null;
+    }
+
+    public static void mockAuthorize(User user) {
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                        new AuthorizedUser(user), null, user.getRoles()));
+    }
+
+    public static <T> ResultMatcher contentJson(T expected) {
+        return content().json(writeValue(expected));
+    }
+
+
 }
