3f491121802b2e450bcd3875eac5c952e1073a26
Joanna Pakosh <morlineday@ukr.net>
2018-10-16 09:39:13 +0300

    removing redundant jackson dependencies

--- src/main/java/top/graduation/rs/web/json/JacksonConfiguration.java
+++ src/main/java/top/graduation/rs/web/json/JacksonConfiguration.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -21,7 +20,6 @@ public class JacksonConfiguration {
     @Primary
     public static ObjectMapper getMapper() {
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Hibernate5Module());
         mapper.registerModule(new JavaTimeModule());
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
